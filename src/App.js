1377228a76cad481aa1c7cc32c88c9415ed22750
Sai Cherish Potluri <cherish.s.potluri@gmail.com>
2024-12-31 16:19:31 -0600

    added code changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
-import ImageGenerator from "./components/ImageGenerator";
-import Chat from "./components/Chat";
 import RecipeGenerator from "./components/RecipeGenerator";
 import Home from "./components/Home";
 import logo from "./assets/ezgif-4-d77c5f6b87-removebg-preview.png";
+import Trainer from "./components/Trainer";
+import BMI from "./components/BMI";
 
 function App() {
   const [activeTab, setActiveTab] = useState("home"); // Default to "home"
@@ -22,25 +22,26 @@ function App() {
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           {/* Logo Section */}
-          <Navbar.Brand onClick={() => handleTabChange("home")} style={{ cursor: "pointer", display: "flex", alignItems: "center" }}>
-            <img 
-              src={logo} 
-              alt="Bheem Gym Corp Logo" 
-              style={{ height: "40px", marginRight: "10px" }} 
+          <Navbar.Brand
+            onClick={() => handleTabChange("home")}
+            style={{ cursor: "pointer", display: "flex", alignItems: "center" }}
+          >
+            <img
+              src={logo}
+              alt="Bheem Gym Corp Logo"
+              style={{ height: "40px", marginRight: "10px" }}
             />
-            Bheem Gym Corp
+            FACA
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link onClick={() => handleTabChange("chat")}>
-                Chat
-              </Nav.Link>
-              <Nav.Link onClick={() => handleTabChange("image-generator")}>
-                Image Generator
+              <Nav.Link onClick={() => handleTabChange("bmi")}>BMI</Nav.Link>
+              <Nav.Link onClick={() => handleTabChange("trainer")}>
+                Trainer
               </Nav.Link>
               <Nav.Link onClick={() => handleTabChange("recipe-generator")}>
-                Nutritionist
+                Nutrition
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
@@ -50,8 +51,8 @@ function App() {
       {/* Content Section */}
       <div className="content-section">
         {activeTab === "home" && <Home />}
-        {activeTab === "image-generator" && <ImageGenerator />}
-        {activeTab === "chat" && <Chat />}
+        {activeTab === "bmi" && <BMI />}
+        {activeTab === "trainer" && <Trainer />}
         {activeTab === "recipe-generator" && <RecipeGenerator />}
       </div>
     </div>
